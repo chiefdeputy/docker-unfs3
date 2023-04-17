7686ffb926fc158f918d596002af6a016c47a323
chiefdeputy <6720294+chiefdeputy@users.noreply.github.com>
2023-04-17 15:08:47 +0200

    Better job control. Run as user.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM alpine:3.6
+FROM alpine:3.17
 MAINTAINER Nimbix, Inc. <support@nimbix.net>
 
 RUN apk --update add \
     unfs3 \
     rpcbind \
-    e2fsprogs-extra \
+    tzdata \
  && rm -rf /var/cache/apk/*
 
-ADD exports /etc/exports
-ADD docker-entrypoint.sh /usr/local/bin/
+COPY exports /etc/exports
+COPY docker-entrypoint.sh /usr/local/bin/
 
 EXPOSE 111/udp 111/tcp 2049/tcp 2049/udp
 VOLUME /export
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
-rpcbind
-unfsd -d
+
+set -e
+
+USER=user
+
+# Add user and create folders
+if [[ -n $UID ]] && [[ $UID != "0" ]]; then
+  echo "A UID was given!"
+  if ! id user &> /dev/null; then
+    echo "User $UID does not exist. Creating..."
+    if [[ -n $GID ]] && [[ $GID != "0" ]] && [[ $GID != $UID ]]; then
+      adduser --disabled-password --gecos "" --no-create-home --uid "$UID" --ingroup $GID $USER
+    else
+      adduser --disabled-password --gecos "" --no-create-home --uid "$UID" $USER
+    fi
+  fi
+else
+  echo "No UID given..."
+  USER=root
+fi
+
+echo "Starting portmapper..."
+rpcbind -d &
+rpc_pid=$!
+echo "Starting nfs daemon as $USER - $UID ..."
+su $USER -c 'unfsd -d' &
+nfsd_pid=$!
+
+wait -n $rpc_pid $nfsd_pid
+
+# Exit with status of process that exited first
+exit $?
